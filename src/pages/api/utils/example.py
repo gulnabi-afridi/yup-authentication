fef108f4ab5c8b9aa6e8d9f34d6dbf63bd283933
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-07-12 22:55:37 +0500

    api folder set

---
+++ src/pages/api/utils/example.py
@@ -0,0 +1,7 @@
+# Set up (use docker):
+# TODO
+# Return a sample image and test that (with the nextjs route wired)
+
+# Finally integrate imaginairy with the flask app
+
+# see flash set up example to return the image => set up @ %root%/flask
