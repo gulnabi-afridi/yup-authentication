8a49aaa3d5cf98685e4adbcf66178d261942ce71
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-07-12 22:48:15 +0500

    footer developed

---
+++ src/components/Layout/styles.tailwind.ts
@@ -0,0 +1,21 @@
+export const tailwindStyles = {
+  // header styles
+  header: 'absolute inset-x-0 top-0 z-50',
+  nav: 'flex items-center justify-between p-6 lg:px-8',
+  logoLink: '-m-1.5 p-1.5',
+  logoContainer: 'flex lg:flex-1',
+  logoImage: 'h-8 w-auto',
+  loginButton: 'flex justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
+  loginButtonContainer: "flex lg:flex-1 lg:justify-end",
+
+  // footer styles
+  footerContainer: 'bg-white z-50',
+  footer: 'mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8',
+  footerNav: '-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12',
+  footerNavItem: 'pb-6',
+  footerNavItemLink: 'text-sm leading-6 text-gray-600 hover:text-gray-900',
+  copyRight: 'mt-10 text-center text-xs leading-5 text-gray-500',
+  socials: 'mt-10 flex justify-center space-x-10',
+  socialLink: 'text-gray-400 hover:text-gray-500',
+  itemIcon: 'h-6 w-6',
+}
